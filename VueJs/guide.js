cd14df58142a6e5f289e87d05363cf8ca5fb2bf0
dongrui <ddongrui@qq.com>
2018-03-01 16:49:57 +0800

    Vue.js学习笔记 介绍指南和实例--增加实例

--- VueJs/guide.js
+++ VueJs/guide.js
@@ -136,3 +136,85 @@ var watchExampleVM = new Vue({
     )
   }
 })
+
+new Vue({
+  el: 'vif-example',
+  data:{
+    ok:true
+  }
+})
+
+//用 v-for 把一个数组对应为一组元素
+var examplefor = new Vue({
+  el: '#example-vfor',
+  data: {
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
+
+var example2 = new Vue({
+  el: '#example-vfor2',
+  data: {
+    parentMessage: 'Parent',
+    items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+  }
+})
+
+new Vue({
+  el: '#v-for-object',
+  data: {
+    object: {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30
+    }
+  }
+})
+
+
+Vue.component('todo-item', {
+  template: '\
+    <li>\
+      {{ title }}\
+      <button v-on:click="$emit(\'remove\')">X</button>\
+    </li>\
+  ',
+  props: ['title']
+})
+
+new Vue({
+  el: '#todo-list-example',
+  data: {
+    newTodoText: '',
+    todos: [
+      {
+        id: 1,
+        title: 'Do the dishes',
+      },
+      {
+        id: 2,
+        title: 'Take out the trash',
+      },
+      {
+        id: 3,
+        title: 'Mow the lawn'
+      }
+    ],
+    nextTodoId: 4
+  },
+  methods: {
+    addNewTodo: function () {
+      this.todos.push({
+        id: this.nextTodoId++,
+        title: this.newTodoText
+      })
+      this.newTodoText = ''
+    }
+  }
+})
